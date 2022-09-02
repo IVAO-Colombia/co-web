2ae70c5b906bcd6aab73718df42aa326846b1b6e
Edgardo Alvarez <edgardo@livedesign.org>
2022-09-01 22:49:50 -0500

    “update”

--- resources/views/website/theme-1/layout/header.blade.php
+++ resources/views/website/theme-1/layout/header.blade.php
@@ -73,8 +73,8 @@
                                     <li> <a target="_blank"
                                             href="https://wiki.ivao.aero/en/home/training/main/rank_atc">Rangos</a>
                                     </li>
-                                    <li> <a href="./?page=fra">¿Qué aeropuerto puedo controlar?</a> </li>
-                                    <li> <a href="./?page=gca">Guest Controller Approval</a> </li>
+                                    <li> <a href="{{ url('/') }}">¿Qué aeropuerto puedo controlar?</a> </li>
+                                    <li> <a href="{{ url('/') }}">Guest Controller Approval</a> </li>
                                     <li> <a target="_blank"
                                             href="http://www.aerocivil.gov.co/servicios-a-la-navegacion/servicio-de-informacion-aeronautica-ais">Cartas
                                             de Navegación</a> </li>
@@ -87,14 +87,17 @@
                             <li class="dropdown"><a href="#">{{ __('Traning') }}</a>
                                 <ul class="dropdown-menu">
 
-                                    <li><a href="#">{{ __('Charts') }}<span class="badge bg-danger">NEW</span></a></li>
-                                    <li><a href="#">{{ __('Calendar') }}<span class="badge bg-danger">NEW</span></a></li>
+                                    <li><a href="#">{{ __('Charts') }}</a></li>
+                                    <li><a href="#">{{ __('Calendar') }}</a></li>
                                     <li><a href="#">{{ __('General') }}</a>
                                     </li>
 
                                 </ul>
                             </li>
                             <li><a href="http://co.forum.ivao.aero/" target="_blank">{{ __('Forum') }}</a>
+                            </li>
+                            <li class="d-none d-md-block d-sm-block d-xs-block">
+                                <a href="{{ route('ivao.login') }}" }}">Ingresar</a>
                             </li>
                             @if (Auth::check())
                                 <li class="dropdown">
