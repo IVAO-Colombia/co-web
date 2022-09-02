94c5bb7df48ea2e44c73d47206b0e27aeaf8fb08
Edgardo Alvarez <edgardo@livedesign.org>
2022-09-02 08:22:03 -0500

    “update”

--- resources/views/website/theme-1/index.blade.php
+++ resources/views/website/theme-1/index.blade.php
@@ -51,7 +51,7 @@
                 <h2 class="">Eventos</h2>
                 <span class="lead">Nuestros proximos eventos. </span>
             </div>
-            <div id="blog" class="grid-layout post-4-columns m-b-30" data-item="post-item">
+            <div id="blog" class="grid-layout post-3-columns m-b-30" data-item="post-item">
                 <!-- Post item-->
                 <div class="post-item border">
                     <div class="post-item-wrap">
@@ -121,27 +121,7 @@
                     </div>
                 </div>
                 <!-- end: Post item-->
-                <!-- Post item-->
-                <div class="post-item border">
-                    <div class="post-item-wrap">
-                        <div class="post-image">
-                            <a href="#">
-                                <img alt="" src="{{ asset('theme-1/images/blog/18.jpg') }}">
-                            </a>
-                            <span class="post-meta-category"><a href="">Science</a></span>
-                        </div>
-                        <div class="post-item-description">
-                            <span class="post-meta-date"><i class="fa fa-calendar-o"></i>Jan 21, 2017</span>
-                            <span class="post-meta-comments"><a href=""><i class="fa fa-comments-o"></i></a></span>
-                            <h2><a href="#">Standard post with a single image
-                                </a></h2>
-                            <p>Curabitur pulvinar euismod ante, ac sagittis ante posuere ac. Vivamus luctus commodo
-                                dolor porta feugiat. Fusce at velit id ligula pharetra laoreet.</p>
-                            <a href="#" class="item-link">Read More <i class="icon-chevron-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <!-- end: Post item-->
+
             </div>
 
         </div>
