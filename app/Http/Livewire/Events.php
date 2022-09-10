7d9ce4f65b40ecf64cca4e8a860e179b0f580556
Edgardo Alvarez <edgardo@livedesign.org>
2022-09-10 11:27:52 -0500

    “update”

--- app/Http/Livewire/Events.php
+++ app/Http/Livewire/Events.php
@@ -7,11 +7,32 @@ use App\Models\Event;
 
 class Events extends Component
 {
-    public $events;
+    public $events, $title, $slug, $date_text;
+    public $modal = false;
 
     public function render()
     {
         $this->events = Event::all();
         return view("livewire.events.view");
+    }
+
+    public function crear()
+    {
+        $this->clearFields();
+        $this->openModal();
+    }
+
+    public function openModal()
+    {
+        $this->modal = true;
+    }
+
+    public function closeModal()
+    {
+        $this->modal = false;
+    }
+
+    public function clearFields()
+    {
     }
 }
--- resources/views/livewire/events/view.blade.php
+++ resources/views/livewire/events/view.blade.php
@@ -5,29 +5,68 @@
         </h2>
     </x-slot>
 
-    <div class="container mx-auto m-3 p-4">
-        <table class="table-fixed">
-            <thead>
-                <th>
-                <td>
-                    ID
-                </td>
-                <td>
-                    Title
-                </td>
-                </th>
-            </thead>
-            <tbody>
+    <div class="py-12">
 
-                @foreach ($events as $item)
-                    <tr>
-                        <td>{{ $item->id }}</td>
-                        <td>{{ $item->title }}</td>
+    </div>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+
+            @if (session()->has('message'))
+                <div class="bg-teal-100 rounded-b text-teal-900 px-4 py-4 shadow-md my-3" role="alert">
+                    <div class="flex">
+                        <div>
+                            <h4>{{ session('message') }}</h4>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+
+            <button wire:click="crear()"
+                class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 my-3">Nuevo</button>
+            @if ($modal)
+                @include('livewire.events.create')
+            @endif
+
+            <table class="table-fixed w-full my-2">
+                <thead>
+                    <tr class="bg-indigo-600 text-white">
+                        <td class="px-4 py-2">
+                            ID
+                        </td>
+                        <td class="px-4 py-2">
+                            Title
+                        </td>
+                        <td class="px-4 py-2">
+                            Slug
+                        </td>
+                        <td class="px-4 py-2">
+                            Actions
+                        </td>
                     </tr>
-                @endforeach
+                </thead>
+                <tbody>
 
-            </tbody>
-        </table>
+                    @foreach ($events as $item)
+                        <tr>
+                            <td class="border px-4 py-2">{{ $item->id }}</td>
+                            <td class="border px-4 py-2">{{ $item->title }}</td>
+                            <td class="border px-4 py-2">{{ $item->slug }}</td>
+                            <td class="border px-4 py-2 text-center">
+                                <button wire:click="editar({{ $item->id }})"
+                                    class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4">Edit</button>
+                                <button wire:click="borrar({{ $item->id }})"
+                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4">Delete</button>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+
+
+        </div>
+
 
 
     </div>
