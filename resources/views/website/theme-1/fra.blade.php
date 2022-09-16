090c22ad960c364db8b8e0a26fe16dbda080e13c
Edgardo Alvarez <edgardo@livedesign.org>
2022-09-15 21:34:49 -0500

    “update”

---
+++ resources/views/website/theme-1/fra.blade.php
@@ -0,0 +1,32 @@
+@extends('website.theme-1.layout.theme-1')
+@section('content')
+    <!-- Inspiro Slider -->
+    <div id="slider" class="inspiro-slider dots-creative h-40" data-height-xs="360">
+        <!-- Slide 2 -->
+        <div class="slide kenburns" style="background-image:url('{{ asset('img/skmd.jpeg') }}');">
+            <div class="bg-overlay"></div>
+            <div class="container">
+                <div class="slide-captions text-center text-light">
+                    <!-- Captions -->
+                    <span class="strong">FRA</span>
+                    <h1> <b>¿Cual aeropuerto puedo controlar? </b></h1>
+                    <!-- end: Captions -->
+                </div>
+            </div>
+        </div>
+        <!-- end: Slide 2 -->
+
+    </div>
+    <!--end: Inspiro Slider -->
+
+
+    <section>
+        <div class="container">
+            <div class="row ">
+                <div class="col-12 mb-2">
+
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
