9c0ba1a4597301028af3c9efc19a7874ba4dad9f
Edgardo Alvarez <edgardo@livedesign.org>
2022-09-28 17:57:00 -0500

    “update”

--- app/Http/Livewire/Admin/Trainings.php
+++ app/Http/Livewire/Admin/Trainings.php
@@ -3,11 +3,15 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Training;
 
 class Trainings extends Component
 {
     public function render()
     {
-        return view('livewire.admin.trainings');
+        return view("livewire.admin.trainings.view", [
+            "trainingcreated" => Training::where("status", 1)->paginate(10),
+            "trainingassigned" => Training::where("status", 2)->paginate(10),
+        ]);
     }
 }
