4df8449ce786db5b522794b168214bc79d754b23
Edgardo Alvarez <edgardo@livedesign.org>
2022-09-30 22:31:47 -0500

    “update”

--- resources/views/livewire/admin/virtualairlines/tracker_airline.blade.php
+++ resources/views/livewire/admin/virtualairlines/tracker_airline.blade.php
@@ -23,14 +23,14 @@
                                     <td
                                         class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
                                         @php
-                                            $week_start = (new DateTime())->setISODate(date('Y'), $item->week)->format('Y-m-d H:i:s');
+                                            $week_start = (new DateTime())->setISODate(date('Y'), $item->week)->format('Y-m-d');
 
-                                            $start = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $week_start);
+                                            $start = \Carbon\Carbon::createFromFormat('Y-m-d', $week_start);
 
-                                            $start
-                                                ->hour(0)
-                                                ->minute(0)
-                                                ->second(0);
+                                            // $start
+                                            //     ->hour(0)
+                                            //     ->minute(0)
+                                            //     ->second(0);
 
                                             $end = $start->copy()->endOfWeek();
                                         @endphp
