1f786c6e62d8f13a322a5d38da986534ca8df48a
Edgardo Alvarez <edgardo@livedesign.org>
2022-10-03 13:05:49 -0500

    “update”

--- resources/views/livewire/admin/virtualairlines/tracker_airline.blade.php
+++ resources/views/livewire/admin/virtualairlines/tracker_airline.blade.php
@@ -48,9 +48,7 @@
                                     <td
                                         class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
                                         {{ secondsToHours($item->secondFlight, true) }}
-                                        {{ $item->secondFlight .
-                                            "
-                                                                                Seconds" }}
+                                        {{-- {{ $item->secondFlight . 'Seconds' }} --}}
                                     </td>
                                 </tr>
                             @endforeach
