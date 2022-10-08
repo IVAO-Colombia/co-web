3880e3fa7ddc3ff6a002902cea8e73021b0bb769
rcomunica <julir2772@gmail.com>
2022-10-07 21:41:08 -0500

    GCA_ES finish

--- resources/views/website/theme-1/gca.blade.php
+++ resources/views/website/theme-1/gca.blade.php
@@ -250,11 +250,61 @@
                             control y conocimiento de regulaciones locales, por lo que no se maneja un puntaje, el resultado
                             es la valoración del evaluador.
                         </p>
+
+                        <h4 class=" "> Resultado Final</h4>
+                        <p>
+                            Al finalizar el chequeo el evaluador informará al aspirante si el resultado fue o no satisfactorio, si lo fue, 
+                            el examinador notificará a los coordinadores del departamento de entrenamiento el resultado para que el GCA se haga efectivo, 
+                            esto se realizará en un plazo máximo de 15 días calendario a partir del día siguiente del chequeo.
+                            <br><br>
+                            Por parte de la no aprobación del chequeo, el aspirante deberá esperar 15 días calendario a partir del día siguiente del chequeo para solicitar una nueva evaluación, 
+                            en este caso se omite la parte de entrenamiento, esta quedará a decisión del entrenador si decide o no brindarla posterior al chequeo insatisfactorio.
+                            <br><br>
+                            <b>Nota:</b> El aspirante deberá esperar el correo de confirmación del GCA por parte de la coordinación del departamento de entrenamiento para poder conectarse en las 
+                            dependencias del país, si incurre en la falta de esta regulación, el miembro no obtendrá su GCA y deberá esperar 90 días calendario para enviar su solicitud de nuevo.    
+                        </p>
+                        
+                        <h4 class=" ">Reglas para mantener el GCA</h4>
+
+                        <ul class="list-disc ml-4 pl-4">
+                                <li>
+                                    <p>
+                                        El usuario poseedor del GCA debe cumplir con mínimo 6 horas de control en un lapso de 30 días calendario, para un total de 18 horas por trimestre. 
+                                        En caso de no cumplir con el mínimo de horas trimestrales, el GCA será removido y deberá esperar mínimo 90 días calendario para realizar una nueva solicitud. 
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Si el usuario incumple alguna de las R&R de IVAO el GCA será removido y 
+                                        el tiempo para enviar una nueva solicitud no podrá ser menor al doble de la sanción establecida por la red.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Si el usuario no presenta conocimiento adecuando de los procedimientos locales y los estándares de control el GCA será removido, 
+                                        el tiempo de espera para una nueva solicitud será determinado por la coordinación del departamento de entrenamiento de la división Colombia y su HQ.
+                                    </p>    
+                                </li>
+                            </ul>
+                        <h4 class=" ">Consideraciones finales</h4>
+                        <p>
+                            El GCA es una herramienta dispuesta por IVAO para aumentar la participación de los miembros e incentivar la relación entre la comunidad, 
+                            sin embargo, queda a discreción del departamento de entrenamiento y el HQ divisional la libre remoción de la habilitación.
+                            <br><br>
+                            Para ex miembros de la división el departamento de entrenamiento, de la mano con el HQ divisional, podrán tomar la decisión de 
+                            otorgar el GCA sin necesidad de chequeo; esto siempre cuando el rango con el que aspire a controlar haya sido obtenido en la división colombiana. 
+                            <br><br>
+                            Así mismo, el departamento de entrenamiento y el HQ divisional tiene la potestad de agregar y remover las habilitaciones a 
+                            las demás dependencias con base en su criterio de evaluación. 
+                            <br><br>
+                            Para miembros con rango APC, poseedores o aspirantes al GCA que estén interesados en controlar las dependencias de control 
+                            superior deberán haber obtenido el rango en la división Colombia.
+                            <br><br>
+                            No es necesario que el aspirante realice el chequeo inicial en la mayor dependencia que le permita su rango, por ejemplo, un miembro con
+                            rango ACC puede solicitar el GCA por medio de un chequeo en torre, sin embargo, debe realizar todo el proceso para las demás dependencias.
+                        </p>
                     </div>
                 </div>
-
-
-
             </div>
         </div>
     </section>
