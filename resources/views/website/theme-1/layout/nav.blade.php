7fa581e91546a5858d0a0c1698a6a3e98a41bb9d
rcomunica <julir2772@gmail.com>
2023-01-14 01:39:03 -0500

    “update”

--- resources/views/website/theme-1/layout/nav.blade.php
+++ resources/views/website/theme-1/layout/nav.blade.php
@@ -55,7 +55,9 @@
                   </li>
                   <li class="dropdown"><a href="#">{{ __('utilities') }}</a>
                     <ul class="dropdown-menu">
+                        @if (Auth::check())
                         <li> <a href="" title="{{__('Any problem? ask for support on our official discord')}}">{{ __('Discord') }}</a>
+                        @endif     
                         <li><a href="http://co.forum.ivao.aero/" target="_blank">{{ __('Forum') }}</a>        
                         <li><a href="https://tours.at.ivao.aero/index.php?div=CO" target="_blank">{{ __('Tours') }}</a>
                         <li><a href="https://eaip-colombia.atnaerocivil.gov.co/eaip/AIRAC%20AMDT%2064-22_2022_12_29/index.html" target="_blank">{{ __('eAIP') }}</a>
