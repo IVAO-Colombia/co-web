b86f85d0430b2c363fe9281169fa7df8adc02a15
santigarcor <7565417+santigarcor@users.noreply.github.com>
2026-04-21 13:23:56 +0000

    fix code style

--- resources/js/pages/events/Show.vue
+++ resources/js/pages/events/Show.vue
@@ -37,7 +37,6 @@ import TooltipProvider from '@/components/ui/tooltip/TooltipProvider.vue';
 import TooltipTrigger from '@/components/ui/tooltip/TooltipTrigger.vue';
 import { usePermissions } from '@/composables/usePermissions';
 import { formatDateTime } from '@/lib/utils';
-import { destroy, index, show as showRoute } from '@/routes/events';
 import type { EventDetail } from '@/types';
 import {
     EventConstants,
@@ -45,6 +44,7 @@ import {
     SlotsConstants,
     SlotStatus,
 } from '@/types';
+import { destroy, index, show as showRoute } from '@/routes/events';
 
 const props = defineProps<{
     event: EventDetail;
