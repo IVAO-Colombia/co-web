c669310f1153a7616c54845d26ef21f788fbe23b
rcomunica <julir2772@gmail.com>
2026-08-12 22:43:33 -0500

    FEAT | Add auto change date publish

--- routes/landing.php
+++ routes/landing.php
@@ -7,8 +7,14 @@ use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\Landing\EventsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomePageController::class)->name('home');
-Route::get('/about-us', fn () => inertia(PagesComponents::LANDING_ABOUT_US->value))->name('home.about');
-Route::get('/training', fn () => inertia(PagesComponents::LANDING_TRAINING->value))->name('home.training');
+
+if (time() > strtotime("August 16 2026 12:00:00")) {
+    Route::get('/', HomePageController::class)->name('home');
+} else {
+    Route::view('/', 'countdown')->name('home');
+}
+
+Route::get('/about-us', fn() => inertia(PagesComponents::LANDING_ABOUT_US->value))->name('home.about');
+Route::get('/training', fn() => inertia(PagesComponents::LANDING_TRAINING->value))->name('home.training');
 Route::get('/events', [EventsController::class, 'index'])->name('home.events');
 Route::get('/events/{event:slug}', [EventsController::class, 'show'])->name('home.events.show');
