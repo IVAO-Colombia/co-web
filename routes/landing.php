3b47a8cdd2ab9c4d15862b74ef174feaa903eb8d
rcomunica <90717365+rcomunica@users.noreply.github.com>
2026-08-13 03:45:33 +0000

    fix code style

--- routes/landing.php
+++ routes/landing.php
@@ -7,14 +7,13 @@ use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\Landing\EventsController;
 use Illuminate\Support\Facades\Route;
 
-
-if (time() > strtotime("August 16 2026 12:00:00")) {
+if (time() > strtotime('August 16 2026 12:00:00')) {
     Route::get('/', HomePageController::class)->name('home');
 } else {
     Route::view('/', 'countdown')->name('home');
 }
 
-Route::get('/about-us', fn() => inertia(PagesComponents::LANDING_ABOUT_US->value))->name('home.about');
-Route::get('/training', fn() => inertia(PagesComponents::LANDING_TRAINING->value))->name('home.training');
+Route::get('/about-us', fn () => inertia(PagesComponents::LANDING_ABOUT_US->value))->name('home.about');
+Route::get('/training', fn () => inertia(PagesComponents::LANDING_TRAINING->value))->name('home.training');
 Route::get('/events', [EventsController::class, 'index'])->name('home.events');
 Route::get('/events/{event:slug}', [EventsController::class, 'show'])->name('home.events.show');
